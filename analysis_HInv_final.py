d02ac0d6b417d08211bafd84392119107fc20a0c
ChristianTMWeber <c.weber@cern.ch>
2023-02-23 20:36:26 +0100

    Start with the ability to make a histogram

---
+++ analysis_HInv_final.py
@@ -0,0 +1,58 @@
+# run as: fccanalysis final analysis_HInv_final.py
+
+#Input directory where the files produced at the pre-selection level are
+inputDir  = "outputs_HInv/stage1"
+
+#Input directory where the files produced at the pre-selection level are
+outputDir  = "outputs_HInv/final/"
+
+
+#Mandatory: List of processes
+processList = {
+    #'p8_ee_ZZ_ecm240':{},#Run the full statistics in one output file named <outputDir>/p8_ee_ZZ_ecm240.root
+    #'p8_ee_WW_ecm240':{'fraction':0.5, 'chunks':2}, #Run 50% of the statistics in two files named <outputDir>/p8_ee_WW_ecm240/chunk<N>.root
+    'wzp6_ee_mumuH_ecm240':{} #Run over the full statistics from the previous stage's input file <inputDir>/p8_ee_ZH_ecm240_out.root. 
+}
+
+
+#Link to the dictonary that contains all the cross section informations etc...
+procDict = "FCCee_procDict_winter2023_IDEA.json"
+
+#Add MySample_p8_ee_ZH_ecm240 as it is not an offical process
+#procDictAdd={"wzp6_ee_mumuH_ecm240":{"numberOfEvents": 10000000, "sumOfWeights": 10000000, "crossSection": 0.201868, "kfactor": 1.0, "matchingEfficiency": 1.0}}
+
+#Number of CPUs to use
+nCPUS = 2
+
+#produces ROOT TTrees, default is False
+doTree = False
+
+###Dictionnay of the list of cuts. The key is the name of the selection that will be added to the output file
+cutList = {
+    "sel0" : "1",
+    #"sel1" : "selected_jets_pt_0 > 20",
+    #"sel2" : "selected_jets_pt_0 > 30",
+    #"sel3" : "selected_jets_pt_0 > 40",
+    #"sel4" : "selected_jets_pt_0 > 50",
+
+    #"sel0" : "Zcand_m > 40 && Zcand_m < 120",
+}
+
+#Dictionary for the ouput variable/hitograms. The key is the name of the variable in the output files. "name" is the name of the variable in the input file, "title" is the x-axis label of the histogram, "bin" the number of bins of the histogram, "xmin" the minimum x-axis value and "xmax" the maximum x-axis value.
+#histoList = {
+#    "mz":{"name":"Zcand_m","title":"m_{Z} [GeV]","bin":125,"xmin":0,"xmax":250},
+#    "mz_zoom":{"name":"Zcand_m","title":"m_{Z} [GeV]","bin":40,"xmin":80,"xmax":100},
+#    "leptonic_recoil_m":{"name":"Zcand_recoil_m","title":"Z leptonic recoil [GeV]","bin":100,"xmin":0,"xmax":200},
+#    "leptonic_recoil_m_zoom":{"name":"Zcand_recoil_m","title":"Z leptonic recoil [GeV]","bin":200,"xmin":80,"xmax":160},
+#    "leptonic_recoil_m_zoom1":{"name":"Zcand_recoil_m","title":"Z leptonic recoil [GeV]","bin":100,"xmin":120,"xmax":140},
+#    "leptonic_recoil_m_zoom2":{"name":"Zcand_recoil_m","title":"Z leptonic recoil [GeV]","bin":200,"xmin":120,"xmax":140},
+#    "leptonic_recoil_m_zoom3":{"name":"Zcand_recoil_m","title":"Z leptonic recoil [GeV]","bin":400,"xmin":120,"xmax":140},
+#    "leptonic_recoil_m_zoom4":{"name":"Zcand_recoil_m","title":"Z leptonic recoil [GeV]","bin":800,"xmin":120,"xmax":140},
+#    "leptonic_recoil_m_zoom5":{"name":"Zcand_recoil_m","title":"Z leptonic recoil [GeV]","bin":2000,"xmin":120,"xmax":140},
+#    "leptonic_recoil_m_zoom6":{"name":"Zcand_recoil_m","title":"Z leptonic recoil [GeV]","bin":100,"xmin":130.3,"xmax":132.5},
+#}
+
+#Dictionary for the ouput variable/hitograms. The key is the name of the variable in the output files. "name" is the name of the variable in the input file, "title" is the x-axis label of the histogram, "bin" the number of bins of the histogram, "xmin" the minimum x-axis value and "xmax" the maximum x-axis value.
+histoList = {
+    "mu_pT":{"name":"muons_pt","title":"mu pT [GeV]","bin":100,"xmin":0,"xmax":-1}
+    }
---
+++ analysis_HInv_stage1.py
@@ -0,0 +1,82 @@
+# run as: fccanalysis run analysis_HInv_stage1.py
+
+#Mandatory: List of processes
+processList = {
+    #'p8_ee_ZZ_ecm240':{},#Run the full statistics in one output file named <outputDir>/p8_ee_ZZ_ecm240.root
+    #'p8_ee_WW_ecm240':{'fraction':0.5, 'chunks':2}, #Run 50% of the statistics in two files named <outputDir>/p8_ee_WW_ecm240/chunk<N>.root
+    'wzp6_ee_mumuH_ecm240':{'fraction':0.2, 'output':'wzp6_ee_mumuH_ecm240'} #Run 20% of the statistics in one file named <outputDir>/p8_ee_ZH_ecm240_out.root (example on how to change the output name)
+}
+
+#Mandatory: Production tag when running over EDM4Hep centrally produced events, this points to the yaml files for getting sample statistics
+prodTag     = "FCCee/winter2023/IDEA/"
+
+#Optional: output directory, default is local running directory
+outputDir   = "outputs_HInv/stage1"
+
+#Optional: analysisName, default is ""
+#analysisName = "My Analysis"
+
+#Optional: ncpus, default is 4
+nCPUS       = 2
+
+#Optional running on HTCondor, default is False
+#runBatch    = False
+
+#Optional batch queue name when running on HTCondor, default is workday
+#batchQueue = "longlunch"
+
+#Optional computing account when running on HTCondor, default is group_u_FCC.local_gen
+#compGroup = "group_u_FCC.local_gen"
+
+#Optional test file
+testFile ="root://eospublic.cern.ch//eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_mumuH_ecm240/events_017670037.root"
+
+
+#Mandatory: RDFanalysis class where the use defines the operations on the TTree
+class RDFanalysis():
+
+    #__________________________________________________________
+    #Mandatory: analysers funtion to define the analysers to process, please make sure you return the last dataframe, in this example it is df2
+    def analysers(df):
+        df2 = (
+            df
+            # define an alias for muon index collection
+            .Alias("Muon0", "Muon#0.index")
+            # define the muon collection
+            .Define("muons",  "ReconstructedParticle::get(Muon0, ReconstructedParticles)")
+            # define number of muons
+            .Define("n_muons",  "ReconstructedParticle::get_n( muons ) ")
+            # Filter at on the number of muons
+            .Filter("n_muons==2")
+
+            # define an alias for electron index collection
+            .Alias("Electron0", "Electron#0.index")
+            # define the electron collection
+            .Define("electrons",  "ReconstructedParticle::get(Electron0, ReconstructedParticles)")
+            # define number of electrons
+            .Define("n_electrons",  "ReconstructedParticle::get_n( electrons ) ")
+            # Filter at on the number of electrons
+            .Filter("n_electrons==0")
+
+            .Define("muons_pt", "ReconstructedParticle::get_pt(muons)")
+            # create branch with muon rapidity
+            .Define("muons_y",  "ReconstructedParticle::get_y(muons)")
+            # create branch with muon total momentum
+            .Define("muons_p",   "ReconstructedParticle::get_p(muons)")
+            # create branch with muon energy
+            .Define("muons_e",   "ReconstructedParticle::get_e(muons)")
+
+        )
+        return df2
+
+
+    #__________________________________________________________
+    #Mandatory: output function, please make sure you return the branchlist as a python list
+    def output():
+        branchList = [
+            "muons_pt",
+            "muons_y",
+            "muons_p",
+            "muons_e",
+        ]
+        return branchList
